0dd4355956730c7fde737bb017dd916638e380cd
Nulsea <wm7mw7@gmail.com>
2020-12-17 16:04:57 +0900

    メッセ送信機能の非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="chat-main__message-list__detail">
+          <div class="chat-main__message-list__detail__info">
+            <div class="chat-main__message-list__detail__info__name">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__message-list__detail__info__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="chat-main__message-list__detail__message">
+            ${message.content}
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="chat-main__message-list__detail">
+        <div class="chat-main__message-list__detail__info">
+          <div class="chat-main__message-list__detail__info__name">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__message-list__detail__info__time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__message-list__detail__message">
+          ${message.content}
+        </div>
+      </div>`
+      return html;
+    };
+    
+  }
+
+  $('.Form').on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.chat-main__message-list').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});     
+      
+      $('form')[0].reset();
+      $('.chat-main__message-form__content__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
+    
